1f56a91ac1bf2fd0fa4f4620ad367ea01e5efb21
anxielray <anxielworld@gmail.com>
2024-07-03 08:29:11 +0300

    working cars per minute

--- main.go
+++ main.go
@@ -11,3 +11,10 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) {
 	}
 	panic("CalculateWorkingCarsPerHour not implemented")
 }
+
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) {
+	if successRate > 0 {
+		fmt.Println((productionRate / 60) * int(successRate))
+	}
+	panic("CalculateWorkingCarsPerMinute not implemented")
+}
