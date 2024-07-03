c742a43307a2a195783e64cbaa06f62a6bf91687
anxielray <anxielworld@gmail.com>
2024-07-03 08:28:06 +0300

    working cars per hour

--- main.go
+++ main.go
@@ -11,20 +11,3 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) {
 	}
 	panic("CalculateWorkingCarsPerHour not implemented")
 }
-
-func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) {
-	if successRate > 0 {
-		fmt.Println((productionRate / 60) * int(successRate))
-	}
-	panic("CalculateWorkingCarsPerMinute not implemented")
-}
-
-func CalculateCost(carsCount int) uint {
-	if carsCount > 0 {
-		groups := carsCount / 10
-		individuals := carsCount % 10
-		price := (groups * 95000) + (individuals * 10000)
-		fmt.Println(uint(price))
-	}
-	panic("CalculateCost not implemented")
-}
